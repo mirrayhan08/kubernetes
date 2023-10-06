ac2a81ffdd43f2011d2b9c7c13bec0cd33c0e1ba
mirrayhan08 <mahedi.siu@gmail.com>
2023-10-06 22:26:10 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello, World!</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <?php
+    echo "This message is generated using PHP!";
+    ?>
+</body>
+</html>
